d05e7187d8f6080a2184a9022cc45bdc6b7d9cde
Gregory McCubbin <gwmccubbin@gmail.com>
2018-08-02 09:16:14 -0500

    Video 5

--- contracts/DappTokenCrowdsale.sol
+++ contracts/DappTokenCrowdsale.sol
@@ -2,18 +2,55 @@ pragma solidity 0.4.24;
 
 import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 import "openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 
-contract DappTokenCrowdsale is Crowdsale, MintedCrowdsale {
+contract DappTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale {
+
+  // Track investor contributions
+  uint256 public investorMinCap = 2000000000000000; // 0.002 ether
+  uint256 public investorHardCap = 50000000000000000000; // 50 ether
+  mapping(address => uint256) public contributions;
 
   constructor(
     uint256 _rate,
     address _wallet,
-    ERC20 _token
+    ERC20 _token,
+    uint256 _cap
   )
     Crowdsale(_rate, _wallet, _token)
+    CappedCrowdsale(_cap)
     public
   {
 
+  }
+
+  /**
+  * @dev Returns the amount contributed so far by a sepecific user.
+  * @param _beneficiary Address of contributor
+  * @return User contribution so far
+  */
+  function getUserContribution(address _beneficiary)
+    public view returns (uint256)
+  {
+    return contributions[_beneficiary];
+  }
+
+  /**
+  * @dev Extend parent behavior requiring purchase to respect investor min/max funding cap.
+  * @param _beneficiary Token purchaser
+  * @param _weiAmount Amount of wei contributed
+  */
+  function _preValidatePurchase(
+    address _beneficiary,
+    uint256 _weiAmount
+  )
+    internal
+  {
+    super._preValidatePurchase(_beneficiary, _weiAmount);
+    uint256 _existingContribution = contributions[_beneficiary];
+    uint256 _newContribution = _existingContribution.add(_weiAmount);
+    require(_newContribution >= investorMinCap && _newContribution <= investorHardCap);
+    contributions[_beneficiary] = _newContribution;
   }
 
 }
--- test/DappTokenCrowdsale.test.js
+++ test/DappTokenCrowdsale.test.js
@@ -1,4 +1,5 @@
 import ether from './helpers/ether';
+import EVMRevert from './helpers/EVMRevert';
 
 const BigNumber = web3.BigNumber;
 
@@ -28,11 +29,17 @@ contract('DappTokenCrowdsale', function([_, wallet, investor1, investor2]) {
     // Crowdsale config
     this.rate = 500;
     this.wallet = wallet;
+    this.cap = ether(100);
+
+    // Investor caps
+    this.investorMinCap = ether(0.002);
+    this.inestorHardCap = ether(50);
 
     this.crowdsale = await DappTokenCrowdsale.new(
       this.rate,
       this.wallet,
-      this.token.address
+      this.token.address,
+      this.cap
     );
 
    // Transfer token ownership to crowdsale
@@ -65,6 +72,13 @@ contract('DappTokenCrowdsale', function([_, wallet, investor1, investor2]) {
     });
   });
 
+  describe('capped crowdsale', async function() {
+    it('has the correct hard cap', async function() {
+      const cap = await this.crowdsale.cap();
+      cap.should.be.bignumber.equal(this.cap);
+    });
+  });
+
   describe('accepting payments', function() {
     it('should accept payments', async function() {
       const value = ether(1);
@@ -73,4 +87,53 @@ contract('DappTokenCrowdsale', function([_, wallet, investor1, investor2]) {
       await this.crowdsale.buyTokens(investor1, { value: value, from: purchaser }).should.be.fulfilled;
     });
   });
+
+  describe('buyTokens()', function() {
+    describe('when the contribution is less than the minimum cap', function() {
+      it('rejects the transaction', async function() {
+        const value = this.investorMinCap - 1;
+        await this.crowdsale.buyTokens(investor2, { value: value, from: investor2 }).should.be.rejectedWith(EVMRevert);
+      });
+    });
+
+    describe('when the investor has already met the minimum cap', function() {
+      it('allows the investor to contribute below the minimum cap', async function() {
+        // First contribution is valid
+        const value1 = ether(1);
+        await this.crowdsale.buyTokens(investor1, { value: value1, from: investor1 });
+        // Second contribution is less than investor cap
+        const value2 = 1; // wei
+        await this.crowdsale.buyTokens(investor1, { value: value2, from: investor1 }).should.be.fulfilled;
+      });
+    });
+  });
+
+  describe('when the total contributions exceed the investor hard cap', function () {
+    it('rejects the transaction', async function () {
+      // First contribution is in valid range
+      const value1 = ether(2);
+      await this.crowdsale.buyTokens(investor1, { value: value1, from: investor1 });
+      // Second contribution sends total contributions over investor hard cap
+      const value2 = ether(49);
+      await this.crowdsale.buyTokens(investor1, { value: value2, from: investor1 }).should.be.rejectedWith(EVMRevert);
+    });
+  });
+
+  describe('when the contribution is within the valid range', function () {
+    const value = ether(2);
+    it('succeeds & updates the contribution amount', async function () {
+      await this.crowdsale.buyTokens(investor2, { value: value, from: investor2 }).should.be.fulfilled;
+      const contribution = await this.crowdsale.getUserContribution(investor2);
+      contribution.should.be.bignumber.equal(value);
+    });
+  });
+
+
+
+
+
+
+
+
+
 });
